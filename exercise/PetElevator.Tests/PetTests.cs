d3197b03ed1519721d59cec3d9bdf1f51bd9bf1d
Darya Taraban <daryastaraban@gmail.com>
2023-02-03 12:57:06 -0500

    completed step 4 and unit test

---
+++ exercise/PetElevator.Tests/PetTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PetElevator.CRM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetElevator.Tests
+{
+    
+
+    [TestClass]
+    public class PetTests
+    {
+        private Pet testPet;
+        //private List<string> testVaccines;
+
+        [TestInitialize]
+        public void PetTest()
+        {
+            testPet = new Pet("Laika", "Dog");
+            testPet.Vaccinations = new List<string> { "Rabies", "Distemper", "Parvo" };
+        }
+
+        [TestMethod]
+        public void Pet_Vaccinations_Test_Creating_List()
+        {
+            
+            string expectedResult = "Rabies, Distemper, Parvo";
+            string actualResult = testPet.ListVaccinations();
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+    }
+}
--- exercise/PetElevator/CRM/Pet.cs
+++ exercise/PetElevator/CRM/Pet.cs
@@ -8,7 +8,18 @@ namespace PetElevator.CRM
     {
         public string PetName { get; set; }
         public string Species { get; set; }
-        public List<string> Vaccinations { get; set; }
+        public List<string> Vaccinations { get; set; } = new List<string>();
+
+        public Pet(string petName, string species)
+        {
+            PetName = petName;
+            Species = species;
+        }
+
+        public string ListVaccinations()
+        {
+            return string.Join(", ", Vaccinations);
+        }
 
 
     }
