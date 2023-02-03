f20d512aeb42c6513efb1716d7e7c0c4f021ba00
Darya Taraban <daryastaraban@gmail.com>
2023-02-03 11:37:29 -0500

    added Pet class properties

---
+++ exercise/PetElevator/CRM/Pet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetElevator.CRM
+{
+    public class Pet
+    {
+        public string PetName { get; set; }
+        public string Species { get; set; }
+        public List<string> Vaccinations { get; set; }
+
+
+    }
+}
